644e85b2480e1b045382e2e8dd1d70e6d250f634
SamKunnathSabu <144790132+SamKunnathSabu@users.noreply.github.com>
2023-11-02 16:54:57 +0200

    Update homework_1.c

--- homework/homework_1.c
+++ homework/homework_1.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 int main()
  {   
-  double num1, num2, product;
-  //  user to enter their numbers    
+  long long num1, num2, result;   
   printf("Enter the first number: ");
-  scanf("%lf", &num1);
+  scanf("%lld", "&num1);
   printf("Enter the second number: ");
-  scanf("%lf", &num2);
-
-  product = num1 * num2; 
-  printf("The product of %.2lf and %.2lf is: %.2lf\n", num1, num2, product);
+  scanf("%lld", "&num2);
+  // calculate the result
+  result = num1 * num2; 
+  printf("The multiplication result is: %lld\n", result);
   return 0;
   }
