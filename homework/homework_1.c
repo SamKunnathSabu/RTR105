a981798c6af916a74bf4902a4f48a015309ad498
SamKunnathSabu <144790132+SamKunnathSabu@users.noreply.github.com>
2023-11-02 17:07:32 +0200

    Update homework_1.c

--- homework/homework_1.c
+++ homework/homework_1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
  {   
-  long long num1, num2, result;   
+  long long num1, num2, product;   
   printf("Enter the first number: ");
   scanf("%lld", "&num1);
   printf("Enter the second number: ");
   scanf("%lld", "&num2);
   // calculate the result
-  result = num1 * num2; 
-  printf("The multiplication result is: %lld\n", result);
+  product = num1 * num2; 
+  printf("The multiplication result is: %lld\n", product);
   return 0;
   }
