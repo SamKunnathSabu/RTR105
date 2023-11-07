7a1f7dd9b35bd819f9caf0a75c25f5c8f8a62fb6
SamKunnathSabu <kunnathsam919@gmail.com>
2023-11-07 13:08:29 +0200

    class_013

---
+++ works/ww.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#define N 100// prepared size of sentence
+// for dynamic array try malloc - https://cplusplus.com/reference/cstdlib/malloc/
+
+void statistics();
+int main()
+{
+    char sentence[N];
+    printf("Enter sentence:\n");
+    //scanf("%s", sentence);// problem - scanf takes string only until first space
+    //gets(sentence);// warning by compiler - fgets is better to use
+    //https://cplusplus.com/reference/cstdio/fgets/
+    fgets(sentence,N-1,stdin);
+
+    int n = strlen(sentence);// actual size of sentence (position of first '\0' symbol + 1)
+    printf("Your entered sentence is (%d symbols):\n%s\n",n,sentence);
+
+    for(int i=0;i<n;i++)
+        {
+        //printf("Symbols nr.%02d - %c (ASCII code - %d)\n",i+1,sentence[i],sentence[i]);
+        statistics();
+        }
+
+    printf("\n");
+/*
+    for(int i=0;i<N;i++)
+        {
+        printf("Symbols nr.%02d - %c (ASCII code - %d)\n",i+1,sentence[i],sentence[i]);
+        }
+*/
+    return 0;
+}
+
+void statistics()
+   {
+   ;
+   }
